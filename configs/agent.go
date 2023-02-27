0a06a793a2a5b95cb6489f0b0f5b5b308b0b7646
Ann <unbeman@gmail.com>
2023-02-27 16:38:36 +0300

    add configs dir

---
+++ configs/agent.go
@@ -0,0 +1,42 @@
+package configs
+
+import (
+	"log"
+	"time"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type HttConnectionConfig struct {
+	ClientTimeout time.Duration
+}
+
+func NewHttConnectionConfig() *HttConnectionConfig {
+	return &HttConnectionConfig{ClientTimeout: 5 * time.Second}
+}
+
+type AgentConfig struct {
+	Address        string        `env:"ADDRESS"`
+	PollInterval   time.Duration `env:"POLLINTERVAL"`
+	ReportInterval time.Duration `env:"REPORTINTERVAL"`
+	ReportTimeout  time.Duration
+	Connection     *HttConnectionConfig
+}
+
+func (cfg *AgentConfig) FromEnv() *AgentConfig {
+	if err := env.Parse(cfg); err != nil {
+		log.Fatalln("AgentConfig.FromEnv: can't parse env vars, reason: ", err)
+	}
+	return cfg
+}
+
+func NewAgentConfig() *AgentConfig {
+	cfg := &AgentConfig{
+		Address:        "127.0.0.1:8080",
+		PollInterval:   2 * time.Second,
+		ReportInterval: 10 * time.Second,
+		ReportTimeout:  2 * time.Second,
+		Connection:     NewHttConnectionConfig(),
+	}
+	return cfg
+}
