9e33f1cf490ae5eeb3d49fdc83d4cf4a2009c422
Ann <unbeman@gmail.com>
2023-07-10 20:38:49 +0300

    iter19: added server build info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,7 +14,22 @@ import (
 	"github.com/unbeman/ya-prac-mcas/internal/server"
 )
 
+// Build info variables
+//
+// Example:
+//
+// go run -ldflags "-X 'main.buildVersion=v1.0.0' -X 'main.buildDate=$(date +'%Y/%m/%d %H:%M:%S')' -X 'main.buildCommit=$(git rev-parse HEAD)'" cmd/agent/main.go
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %v\n", buildVersion)
+	fmt.Printf("Build date: %v\n", buildDate)
+	fmt.Printf("Build commit: %v\n", buildCommit)
+
 	cfg := *configs.NewServerConfig(configs.FromFlags(), configs.FromEnv())
 
 	logging.InitLogger(cfg.Logger)
